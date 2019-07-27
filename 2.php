01336b9a35e4e2ad5599a6b7760d61129604dccb
anisavn <48623862+anisavn@users.noreply.github.com>
2019-07-27 20:38:05 +0700

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,8 +1,7 @@
 <?php
     
     function validate_email($email=NULL) {
-    return (preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^/",$email) ? "$email adalah valid" : "$email adalah invalid");  
-    }
+    return (preg_match("/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/",$email) ? "$email adalah valid" : "$email adalah invalid");
 
     echo validate_email("kukuruyuk@gmail.com");
     echo "<br/>" . validate_email("kuk.uruyuk@gmail");
